aa121bae57214d12b0c8f7e0549cc51a46f29840
Nate McDorman <ntmcdorman@gmail.com>
2021-08-01 14:01:47 -0700

    Add files via upload

--- StoryBuilder.go
+++ StoryBuilder.go
@@ -3,7 +3,7 @@ package storybuilder
 import "math/rand"
 
 func GetRoom() string {
-	rooms := [3]string{"Escape", "Ogre", "Trap", "DarkRoom"}
+	rooms := [4]string{"Escape", "Ogre", "Trap", "DarkRoom"}
 	randomIndex := rand.Intn(len(rooms))
 	chosenRoom := rooms[randomIndex]
 	return chosenRoom
