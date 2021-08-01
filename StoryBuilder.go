5695081aa5f8709d7c869331b14699b49bde820b
Nate McDorman <ntmcdorman@gmail.com>
2021-08-01 14:49:46 -0700

    Add files via upload

--- StoryBuilder.go
+++ StoryBuilder.go
@@ -1,11 +1,6 @@
 package storybuilder
 
-import (
-	"math/rand"
-	"time"
-)
-
-func GetRoom() string {
+func GetRoom(index int) string {
 	rooms := [10]string{
 		"ESCAPED! You have found your way out! Congratulations!",
 		"DEATH! The room contains an ogre, unfortunately, it'll kill you regardless of what you do.",
@@ -18,8 +13,9 @@ func GetRoom() string {
 		"A room full of goblins! You're able to fend them off but barely!",
 		"You enter the room, and hear a whisper saying 'Turn back'.",
 	}
-	rand.Seed(time.Now().UnixNano())
-	randomIndex := rand.Intn(len(rooms))
-	chosenRoom := rooms[randomIndex]
-	return chosenRoom
+	return rooms[index]
+}
+
+func GetMaxRooms() int {
+	return 10
 }
