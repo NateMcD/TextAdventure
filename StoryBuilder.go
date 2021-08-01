32dcc81a7f839bc8fa8d5d81cc89078ca5e7909f
Nate McDorman <ntmcdorman@gmail.com>
2021-08-01 13:58:58 -0700

    Add files via upload

--- StoryBuilder.go
+++ StoryBuilder.go
@@ -1,26 +1,10 @@
 package storybuilder
 
-type StoryNode struct {
-	text	string
-	Choices *Choices
-}
+impot "math/rand"
 
-type Choices struct {
-	cmd				string
-	description		string
-	nextNode		*StoryNode
-	nextChoice		*Choices
-}
-
-func (node *StoryNode) addChoice(cmd string, description string, nextNode *StoryNode) {
-	choice := &Choices{cmd, description, nextNode, nil}
-	if node.Choices == nil {
-		node.Choices = choice
-	} else {
-		currentChoice := node.Choices
-		for currentChoice.nextChoice != nil {
-			currentChoice = currentChoice.nextChoice
-		}
-		currentChoice.nextChoice = choice
-	}
+func GetRoom() string {
+	rooms := [3]string{"Escape", "Ogre", "Trap", "DarkRoom"}
+	randomIndex := rand.Intn(len(rooms))
+	chosenRoom := rooms[randomIndex]
+	return chosenRoom
 }
