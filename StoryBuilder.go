2f07f3680c56ee49df55045f68a639bdcbb7b6ca
Nate McDorman <ntmcdorman@gmail.com>
2021-08-01 14:23:03 -0700

    Add files via upload

--- StoryBuilder.go
+++ StoryBuilder.go
@@ -4,9 +4,9 @@ import "math/rand"
 
 func GetRoom() string {
 	rooms := [4]string{
-		"You have found your way out! Congratulations!",
-		"The room contains an ogre, unfortunately, it'll kill you regardless of what you do.",
-		"You encounter a trap, instant death!",
+		"ESCAPED! You have found your way out! Congratulations!",
+		"DEATH! The room contains an ogre, unfortunately, it'll kill you regardless of what you do.",
+		"DEATH! You encounter a trap, instant death!",
 		"You find yourself in a dark room, not sure of your surroundings.",
 	}
 	randomIndex := rand.Intn(len(rooms))
